c1705b8fd28f8ea4a95163b2313120057ed26be4
chaosua <chaosua2008@googlemail.com>
2011-09-16 02:09:17 +0300

    Stealth shouldn't break if damage is absorbed

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14829,6 +14829,10 @@ void Unit::ProcDamageAndSpellFor(bool isVictim, Unit* pTarget, uint32 procFlag, 
                     break;
             }
         }
+        // Stealth shouldn't break if damage is absorbed
+        if ((procFlag & PROC_FLAG_TAKEN_DAMAGE) && !damage)
+           takeCharges = false;
+
         // Remove charge (aura can be removed by triggers)
         if (useCharges && takeCharges)
             i->aura->DropCharge(AURA_REMOVE_BY_EXPIRE);
