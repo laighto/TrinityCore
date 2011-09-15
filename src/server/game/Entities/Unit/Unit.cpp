9faa4f4c901f1f87962bc6cc669b7cd2fe5162ee
chaosua <chaosua2008@googlemail.com>
2011-09-16 01:48:15 +0300

    Glyph of Fear fix

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -6144,6 +6144,22 @@ bool Unit::HandleDummyAuraProc(Unit* victim, uint32 damage, AuraEffect* triggere
                     triggered_spell_id = 37378;
                     break;
                 }
+                //Glyph of Fear 
+                case 42458: 
+                { 
+                    triggered_spell_id = 56244; 
+                    break; 
+                }
+                case 1122: 
+                { 
+                    triggered_spell_id = 22703; 
+                    break; 
+                } 
+                case 18540: 
+                { 
+                    triggered_spell_id = 60478; 
+                    break; 
+                }
             }
             break;
         }
