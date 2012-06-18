5f40cdb28aa7ae5ba0616d182f7b2d1815b2b544
chaosua <chaosua2008@googlemail.com>
2012-06-18 03:19:43 +0300

    Fix Bloodthirst heal 1% of MaxHealth

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9069,7 +9069,7 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffect* trigg
         // Bloodthirst (($m/100)% of max health)
         case 23880:
         {
-            basepoints0 = int32(CountPctFromMaxHealth(triggerAmount));
+            basepoints0 = int32(GetMaxHealth() * 0.01);
             break;
         }
         // Shamanistic Rage triggered spell
