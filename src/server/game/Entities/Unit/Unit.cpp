f7a6cde95a15dcfc28ca65f379138a5849e221b7
chaosua <chaosua2008@googlemail.com>
2012-06-27 01:06:39 +0300

    restore Bloodthirst heal part

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -9066,6 +9066,12 @@ bool Unit::HandleProcTriggerSpell(Unit* victim, uint32 damage, AuraEffect* trigg
                 CastSpell(this, 70721, true);
             break;
         }
+        // Bloodthirst (($m/100)% of max health)
+        case 23880:
+        {
+            basepoints0 = int32(GetMaxHealth() * 0.01);
+            break;
+        }
         // Shamanistic Rage triggered spell
         case 30824:
         {
