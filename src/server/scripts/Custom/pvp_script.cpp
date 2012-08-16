e5d2a06c33e4c3de3f367f958d2c0c3747c2b444
chaosua <chaosua2008@googlemail.com>
2012-08-17 02:14:23 +0300

    make random more harder

--- src/server/scripts/Custom/pvp_script.cpp
+++ src/server/scripts/Custom/pvp_script.cpp
@@ -50,8 +50,11 @@ public:
             uint32 rnd = urand(0,2);
             killed->TeleportTo(0, allyPositions[rnd].GetPositionX(), allyPositions[rnd].GetPositionY(), allyPositions[rnd].GetPositionZ(), allyPositions[rnd].GetOrientation());
            
-            if (urand(1,100) == urand(1,100))
+            if (urand(1,200) == urand(1,200))
+            {
                 killer->AddItem(EMBLEM_OF_FROST, 1);
+                sWorld->SendWorldText(12015, killer->GetName);
+            }
         }
     }
 };
