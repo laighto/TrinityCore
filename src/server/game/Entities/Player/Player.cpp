dff64f7578bf83425f7a49bdc2e63b4bb54149e9
chaosua <chaosua2008@googlemail.com>
2012-08-19 01:19:34 +0300

    Fix transport disapper on grid change Thx @GriffonHeart

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -21807,6 +21807,18 @@ inline void UpdateVisibilityOf_helper(std::set<uint64>& s64, T* target, std::set
 }
 
 template<>
+inline void UpdateVisibilityOf_helper(std::set<uint64>& s64, GameObject* target, std::set<Unit*>& /*v*/)
+{
+    // From old trinity revision
+    //if (!target->IsTransport() && !(target->GetGOInfo()->type == GAMEOBJECT_TYPE_DESTRUCTIBLE_BUILDING))
+    //    s64.insert(target->GetGUID());
+
+    // Don't update only GAMEOBJECT_TYPE_TRANSPORT (or all transports and destructible buildings?)
+    if (!(target->GetGOInfo()->type == GAMEOBJECT_TYPE_TRANSPORT))
+        s64.insert(target->GetGUID());
+}
+
+template<>
 inline void UpdateVisibilityOf_helper(std::set<uint64>& s64, Creature* target, std::set<Unit*>& v)
 {
     s64.insert(target->GetGUID());
