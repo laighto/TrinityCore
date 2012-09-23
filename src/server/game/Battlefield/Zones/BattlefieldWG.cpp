813200ddd9aeaba02d602c7e6a00e71213779610
chaosua <chaosua2008@googlemail.com>
2012-09-23 12:46:08 +0300

    Add Support of Quests Toppling the Towers & Southern Sabotage

--- src/server/game/Battlefield/Zones/BattlefieldWG.cpp
+++ src/server/game/Battlefield/Zones/BattlefieldWG.cpp
@@ -980,7 +980,17 @@ void BattlefieldWG::ProcessEvent(WorldObject *obj, uint32 eventId)
                 (*itr)->Damaged();
 
             if ((*itr)->m_Build->GetGOInfo()->building.destroyedEvent == eventId)
+            {
                 (*itr)->Destroyed();
+
+                // Add Support of Quests Toppling the Towers & Southern Sabotage
+                if (go->GetEntry()==190356 || go->GetEntry()==190357 || go->GetEntry()==190358)
+                {
+                    for (GuidSet::const_iterator itr = m_PlayersInWar[GetDefenderTeam()].begin(); itr != m_PlayersInWar[GetDefenderTeam()].end(); ++itr)
+                        if (Player* player = sObjectAccessor->FindPlayer(*itr))
+                            player->RewardPlayerAndGroupAtEvent(35074, go);
+                }
+            }
 
             break;
         }
