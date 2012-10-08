e7b989632f14719226107f45179e548d1905437e
chaosua <chaosua2008@googlemail.com>
2012-10-08 03:31:59 +0300

    ruby sanctum fix flags on halion

--- src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
+++ src/server/scripts/Northrend/ChamberOfAspects/RubySanctum/boss_halion.cpp
@@ -307,7 +307,7 @@ class boss_halion : public CreatureScript
                 instance->SendEncounterUnit(ENCOUNTER_FRAME_DISENGAGE, me);
 
                 me->RemoveAurasDueToSpell(SPELL_TWILIGHT_PHASING);
-                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
+                me->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE | UNIT_FLAG_IMMUNE_TO_PC | UNIT_FLAG_NON_ATTACKABLE);
             }
 
             void EnterEvadeMode()
