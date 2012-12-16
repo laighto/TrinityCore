07e77a6c727cc8ccd2cdc01c7c604811811aabc7
chaosua <chaosua2008@googlemail.com>
2012-12-16 20:09:50 +0200

    some hack

--- src/server/scripts/Outland/shadowmoon_valley.cpp
+++ src/server/scripts/Outland/shadowmoon_valley.cpp
@@ -737,12 +737,12 @@ public:
             Step = 0;
 
             Event = false;
-            me->SetUInt32Value(UNIT_NPC_FLAGS, 2);
+            me->SetUInt32Value(UNIT_NPC_FLAGS, 3);
         }
 
         void StartEvent()
         {
-            me->SetUInt32Value(UNIT_NPC_FLAGS, 0);
+            me->SetUInt32Value(UNIT_NPC_FLAGS, 2);
             me->SetUInt32Value(UNIT_FIELD_BYTES_1, 0);
             Unit* Illidan = me->SummonCreature(C_ILLIDAN, -5107.83f, 602.584f, 85.2393f, 4.92598f, TEMPSUMMON_CORPSE_DESPAWN, 0);
             if (Illidan)
