3c4b732df133b8ce6c8baa7703d9e26e5ab92afb
chaosua <chaosua2008@googlemail.com>
2013-02-19 02:39:07 +0200

    rework necrotic plague

--- src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
@@ -2105,17 +2105,15 @@ class spell_the_lich_king_necrotic_plague : public SpellScriptLoader
 
 
                 CustomSpellValues values;
-                if(used_dispell)
+                if (used_dispell == true)
                 {
                     used_dispell = false;
 
-                    if ((GetStackAmount() - 1) < 1)
-                        return;
-                    else
-                    {
+                    if ((GetStackAmount() - 1) > 1)
                         values.AddSpellMod(SPELLVALUE_AURA_STACK, GetStackAmount() - 1);
-                        values.AddSpellMod(SPELLVALUE_RADIUS_MOD, 10);
-                    }
+
+                    values.AddSpellMod(SPELLVALUE_RADIUS_MOD, 10);
+
                 }
                 values.AddSpellMod(SPELLVALUE_MAX_TARGETS, 1);
                 GetTarget()->CastCustomSpell(SPELL_NECROTIC_PLAGUE_JUMP, values, NULL, TRIGGERED_FULL_MASK, NULL, NULL, GetCasterGUID());
@@ -2232,7 +2230,12 @@ class spell_the_lich_king_necrotic_plague_jump : public SpellScriptLoader
 
                 CustomSpellValues values;
                 values.AddSpellMod(SPELLVALUE_MAX_TARGETS, 1);
-                values.AddSpellMod(SPELLVALUE_AURA_STACK, GetStackAmount());
+
+                if ((GetStackAmount() - 1) > 1)
+                    values.AddSpellMod(SPELLVALUE_AURA_STACK, GetStackAmount() - 1);
+                else
+                    values.AddSpellMod(SPELLVALUE_AURA_STACK, GetStackAmount());
+
                 values.AddSpellMod(SPELLVALUE_BASE_POINT1, AURA_REMOVE_BY_ENEMY_SPELL); // add as marker (spell has no effect 1)
                 GetTarget()->CastCustomSpell(SPELL_NECROTIC_PLAGUE_JUMP, values, NULL, TRIGGERED_FULL_MASK, NULL, NULL, GetCasterGUID());
                 if (Unit* caster = GetCaster())
