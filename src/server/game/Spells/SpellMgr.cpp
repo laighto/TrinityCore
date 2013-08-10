ce6beb08f7899191e1e616d4a040820c055da0d8
chaosua <chaosua2008@gmail.com>
2013-08-10 15:29:48 +0300

    spellfocus

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3754,7 +3754,7 @@ void SpellMgr::LoadSpellInfoCorrections()
                 break;
             //Server custom
             case 66795:
-                spellInfo->Effects[EFFECT_1].TargetA = SpellImplicitTargetInfo(TARGET_SRC_CASTER | TARGET_GAMEOBJECT_SRC_AREA);
+                spellInfo->Effects[EFFECT_1].TargetA = SpellImplicitTargetInfo(TARGET_GAMEOBJECT_SRC_AREA);
                 break;
             default:
                 break;
