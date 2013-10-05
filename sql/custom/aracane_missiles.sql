061776f0584440134634253d986e52ce65a7e324
chaosua <chaosua2008@gmail.com>
2013-10-05 20:54:24 +0300

    fix arcane missiles proc chance

--- sql/custom/aracane_missiles.sql
+++ sql/custom/aracane_missiles.sql
@@ -4,3 +4,7 @@ INSERT INTO `spell_linked_spell` VALUES
 (-79683, -79808, 0, 'Arcane Missiles aurastate link'),
 -- On retail, you lose the 79683 aura by just clicking on arcane missiles
 (5143,-79683,0,'Arcane missiles! aura removal due to arcane missiles cast');
+
+-- Fixed proc chance
+DELETE FROM `spell_proc_event` WHERE `entry`=79684;
+INSERT INTO `spell_proc_event` (`entry`, `CustomChance`) VALUES ('79684', '40');
