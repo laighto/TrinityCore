3b9b71d9088280a4c337b04bbf80776340c4fc75
chaosua <chaosua2008@gmail.com>
2013-10-13 15:14:06 +0300

    Implemented chat mod: channel factions thx Vertraut

--- src/server/scripts/Custom/custom_scripts.cpp
+++ src/server/scripts/Custom/custom_scripts.cpp
@@ -250,10 +250,27 @@ class go_abandoned_outhouse : public GameObjectScript
         }
 };
 
+class channel_factions : public PlayerScript
+{
+     public:
+         channel_factions() : PlayerScript("channel_factions") { }
+
+     void OnChat(Player* player, uint32 /*type*/, uint32 lang, std::string& msg, Channel* channel)
+     {
+         if (!player || !channel)
+             return;
+
+         std::stringstream ssMsg;
+         ssMsg << ((player->GetTeam() == HORDE) ? "|TInterface\\PVPFrame\\PVP-Currency-Horde:18:18:-3:-3|t" : "|TInterface\\PVPFrame\\PVP-Currency-Alliance:18:18:-3:-3|t") << msg;
+         msg = ssMsg.str();
+     }
+};
+
 void AddSC_custom_script()
 {
     new mob_ressurect();
     new mod_afk();
     new Boss_Announcer();
     new go_abandoned_outhouse();
+    new channel_factions();
 }
