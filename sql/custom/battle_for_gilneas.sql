5e64d0b682c9b80a1b172541314a1c8e021703cb
chaosua <chaosua2008@gmail.com>
2013-10-18 12:39:04 +0300

    BFGC: added sound to the text speech

--- sql/custom/battle_for_gilneas.sql
+++ sql/custom/battle_for_gilneas.sql
@@ -25,26 +25,26 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `comment`
 
 
 DELETE FROM creature_text WHERE entry='38218';
-INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `comment`) VALUES 
-('38218', '1', '0',  'The Forsaken think we\'re weak. A broken people. They think we\'ll roll over like a scared dog.', '14', 'q24904'),
-('38218', '2', '0',  'How wrong they are. We will fight them in the fields until the last trench collapses and the last cannon is silenced.', '14', 'q24904'),
-('38218', '3', '0',  'We will fight them on the streets until the last shot is fired. And when there\'s no more ammunition, we\'ll crush their skulls with the stones that pave our city.', '14', 'q24904'),
-('38218', '4', '0',  'We will fight them in the alleys, until our knuckles are skinned and bloody and our rapiers lay on the ground shattered.', '14', 'q24904'),
-('38218', '5', '0',  'And if we find ourselves surrounded and disarmed... wounded and without hope... we will lift our heads in defiance and spit in their faces.', '14', 'q24904'),
-('38218', '6', '0',  'But we will... NEVER SURRENDER!!!!!', '14', 'q24904'),
-('38218', '7', '0',  'FOR GILNEAS!!!', '14', 'q24904'),
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `sound`, `comment`) VALUES 
+('38218', '1', '0',  'The Forsaken think we\'re weak. A broken people. They think we\'ll roll over like a scared dog.', '14', '19623', 'q24904'),
+('38218', '2', '0',  'How wrong they are. We will fight them in the fields until the last trench collapses and the last cannon is silenced.', '14', '19624', 'q24904'),
+('38218', '3', '0',  'We will fight them on the streets until the last shot is fired. And when there\'s no more ammunition, we\'ll crush their skulls with the stones that pave our city.', '14', '19625', 'q24904'),
+('38218', '4', '0',  'We will fight them in the alleys, until our knuckles are skinned and bloody and our rapiers lay on the ground shattered.', '14', '19626', 'q24904'),
+('38218', '5', '0',  'And if we find ourselves surrounded and disarmed... wounded and without hope... we will lift our heads in defiance and spit in their faces.', '14', '19627', 'q24904'),
+('38218', '6', '0',  'But we will... NEVER SURRENDER!!!!!', '14', '19628', 'q24904'),
+('38218', '7', '0',  'FOR GILNEAS!!!', '14', '', 'q24904'),
 --
-('38218', '8', '0',  'You\'re a sight for sore eyes, Lorna. Let\'s get those cannons manned! The rest of you, lure the abominations into the open!', '14', 'q24904'),
-('38218', '9', '0',  'Crowley\'s troops are up ahead! Press on!', '14', 'q24904'),
-('38218', '10', '0',  'FATHER!!!', '14', 'q24904'),
+('38218', '8', '0',  'You\'re a sight for sore eyes, Lorna. Let\'s get those cannons manned! The rest of you, lure the abominations into the open!', '14', '', 'q24904'),
+('38218', '9', '0',  'Crowley\'s troops are up ahead! Press on!', '14', '', 'q24904'),
+('38218', '10', '0',  'FATHER!!!', '14', '', 'q24904'),
 --
-('38218', '11', '0',  'Your time is up, Forsaken scum!', '12', 'q24904'),
-('38218', '11', '1',  'Push them back!', '12', 'q24904'),
-('38218', '11', '2',  'Attack!', '12', 'q24904'),
-('38218', '12', '0',  'Abominations are blocking the way towards the military district! This won\'t be easy.', '12', 'q24904'),
-('38218', '13', '0',  'We took back our city... we took back...', '12', 'q24904'),
-('38218', '14', '0',  'We did it, father...', '12', 'q24904'),
-('38218', '15', '0',  'Gilneas will prevail!', '12', 'q24904');
+('38218', '11', '0',  'Your time is up, Forsaken scum!', '12', '', 'q24904'),
+('38218', '11', '1',  'Push them back!', '12', '', 'q24904'),
+('38218', '11', '2',  'Attack!', '12', '', 'q24904'),
+('38218', '12', '0',  'Abominations are blocking the way towards the military district! This won\'t be easy.', '12', '', 'q24904'),
+('38218', '13', '0',  'We took back our city... we took back...', '12', '', 'q24904'),
+('38218', '14', '0',  'We did it, father...', '12', '', 'q24904'),
+('38218', '15', '0',  'Gilneas will prevail!', '12', '', 'q24904');
 
 DELETE FROM locales_creature_text WHERE entry='38218';
 INSERT INTO `locales_creature_text`  (`entry`, `groupid`, `id`,`text_loc1`, `text_loc2`, `text_loc3`, `text_loc4`, `text_loc5`, `text_loc6`, `text_loc7`, `text_loc8`) VALUES 
