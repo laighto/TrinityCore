debabfb82096a0358ac2c516972db6cbba774937
chaosua <chaosua2008@gmail.com>
2014-05-01 16:45:36 +0300

    Quest the warchief cometh - add sound ids to event

--- sql/custom/the_warchief_cometh.sql
+++ sql/custom/the_warchief_cometh.sql
@@ -8,18 +8,18 @@ UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_w_fallen_human
 UPDATE `creature_template` SET `AIName` = '', `ScriptName` = 'npc_w_elite' WHERE `entry` = 44636;
 
 DELETE FROM creature_text WHERE entry='44365';
-INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `comment`) VALUES 
-('44365', '0', '0',  'Where is that ogre-headed buffoon?', '12', 'q26965 - text1' ),
-('44365', '1', '0',  'Ah, speak of the devil...', '12', 'q26965 - text2'),
-('44365', '2', '0',  'Warchief, so glad you could make it.', '12', 'q26965 - textevent 5'),
-('44365', '3', '0',  'With the death of the Lich King, many of the more intelligent Scourge became... unemployed. Those \'fiends,\' as you so delicately put it, are called val\'kyr. They are under my command now...', '12', 'q26965 - textevent 6'),
-('44365', '4', '0',  '...and they are part of the reason that I asked to see you.', '12', 'q26965 - textevent 7'),
-('44365', '5', '0',  'Very well, Warchief. I have solved the plight of the Forsaken!', '12', 'q26965 - textevent 9'),
-('44365', '6', '0',  'As a race, we Forsaken are unable to procreate.', '12', 'q26965 - textevent 10'),
-('44365', '7', '0',  'With the aid of the val\'kyr, we are now able to take the corpses of the fallen and create new Forsaken.', '12', 'q26965 - textevent 11'),
-('44365', '8', '0',  'Agatha, show the Warchief!', '12', 'q26965 - textevent 12'),
-('44365', '9', '0',  'Warchief, without these new Forsaken my people would die out... Our hold upon Gilneas and northern Lordaeron would crumble.', '12', 'q26965 - textevent 15'),
-('44365', '10', '0',  'Isn\'t it obvious, Warchief? I serve the Horde.', '12', 'q26965 - textevent 18');
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`,  `sound`, `comment`) VALUES 
+('44365', '0', '0',  'Where is that ogre-headed buffoon?', '12', '20459', 'q26965 - text1' ),
+('44365', '1', '0',  'Ah, speak of the devil...', '12', '20460', 'q26965 - text2'),
+('44365', '2', '0',  'Warchief, so glad you could make it.', '12', '20461', 'q26965 - textevent 5'),
+('44365', '3', '0',  'With the death of the Lich King, many of the more intelligent Scourge became... unemployed. Those \'fiends,\' as you so delicately put it, are called val\'kyr. They are under my command now...', '12', '20462', 'q26965 - textevent 6'),
+('44365', '4', '0',  '...and they are part of the reason that I asked to see you.', '12', '20463', 'q26965 - textevent 7'),
+('44365', '5', '0',  'Very well, Warchief. I have solved the plight of the Forsaken!', '12', '20464', 'q26965 - textevent 9'),
+('44365', '6', '0',  'As a race, we Forsaken are unable to procreate.', '12', '20465', 'q26965 - textevent 10'),
+('44365', '7', '0',  'With the aid of the val\'kyr, we are now able to take the corpses of the fallen and create new Forsaken.', '12', '20466', 'q26965 - textevent 11'),
+('44365', '8', '0',  'Agatha, show the Warchief!', '12', '20467', 'q26965 - textevent 12'),
+('44365', '9', '0',  'Warchief, without these new Forsaken my people would die out... Our hold upon Gilneas and northern Lordaeron would crumble.', '12', '20468', 'q26965 - textevent 15'),
+('44365', '10', '0',  'Isn\'t it obvious, Warchief? I serve the Horde.', '12', '20469', 'q26965 - textevent 18');
 
 DELETE FROM locales_creature_text WHERE entry='44365';
 INSERT INTO `locales_creature_text`  (`entry`, `groupid`, `id`,`text_loc1`, `text_loc2`, `text_loc3`, `text_loc4`, `text_loc5`, `text_loc6`, `text_loc7`, `text_loc8`) VALUES 
@@ -36,17 +36,17 @@ INSERT INTO `locales_creature_text`  (`entry`, `groupid`, `id`,`text_loc1`, `tex
 ('44365', '10', '0', NULL,  NULL, NULL, NULL, NULL, NULL, NULL, 'Как это чем, Вождь? Я служу Орде.');
 
 DELETE FROM creature_text WHERE entry='44629';
-INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `comment`) VALUES 
-('44629', '0', '0',  'This better be important, Sylvanas. You know how I detest this place and its foul stench. Why have you called for me?', '12', 'q26965 - text3' ),
-('44629', '1', '0',  'And more importantly, what are those Scourge fiends doing here?', '12', 'q26965 - text4'),
-('44629', '2', '0',  'Get on with it, Sylvanas.', '12', 'q26965 - textevent 8'),
-('44629', '3', '0',  'ABBERATION!', '12', 'q26965 - textevent 13'),
-('44629', '4', '0',  'What you have done here, Sylvanas... it goes against the laws of nature. Disgusting is the only word I have to describe it.', '12', 'q26965 - textevent 14'),
-('44629', '5', '0',  'Have you given any thought to what this means, Sylvanas?', '12', 'q26965 - textevent 16'),
-('44629', '6', '0',  'What difference is there between you and the Lich King now?', '12', 'q26965 - textevent 17'),
-('44629', '7', '0',  'Watch your clever mouth, bitch.', '12', 'q26965 - textevent 19'),
-('44629', '8', '0',  'Cromush, you stay behind and make sure the Banshee Queen is well "guarded." I will be expecting a full report when next we meet.', '12', 'q26965 - textevent 20'),
-('44629', '9', '0',  'Remember, Sylvanas, eventually we all have to stand before our maker and face judgment. Your day may come sooner than others\'....', '12', 'q26965 - textevent 22');
+INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `sound`, `comment`) VALUES 
+('44629', '0', '0',  'This better be important, Sylvanas. You know how I detest this place and its foul stench. Why have you called for me?', '12', '20496', 'q26965 - text3' ),
+('44629', '1', '0',  'And more importantly, what are those Scourge fiends doing here?', '12', '20497', 'q26965 - text4'),
+('44629', '2', '0',  'Get on with it, Sylvanas.', '12', '20498', 'q26965 - textevent 8'),
+('44629', '3', '0',  'ABBERATION!', '12', '', 'q26965 - textevent 13'),
+('44629', '4', '0',  'What you have done here, Sylvanas... it goes against the laws of nature. Disgusting is the only word I have to describe it.', '12', '20499',  'q26965 - textevent 14'),
+('44629', '5', '0',  'Have you given any thought to what this means, Sylvanas?', '12', '20500', 'q26965 - textevent 16'),
+('44629', '6', '0',  'What difference is there between you and the Lich King now?', '12', '20501', 'q26965 - textevent 17'),
+('44629', '7', '0',  'Watch your clever mouth, bitch.', '12', '20502', 'q26965 - textevent 19'),
+('44629', '8', '0',  'Cromush, you stay behind and make sure the Banshee Queen is well "guarded." I will be expecting a full report when next we meet.', '12', '20503', 'q26965 - textevent 20'),
+('44629', '9', '0',  'Remember, Sylvanas, eventually we all have to stand before our maker and face judgment. Your day may come sooner than others\'....', '12', '20504', 'q26965 - textevent 22');
 
 DELETE FROM locales_creature_text WHERE entry='44629';
 INSERT INTO `locales_creature_text`  (`entry`, `groupid`, `id`,`text_loc1`, `text_loc2`, `text_loc3`, `text_loc4`, `text_loc5`, `text_loc6`, `text_loc7`, `text_loc8`) VALUES 
